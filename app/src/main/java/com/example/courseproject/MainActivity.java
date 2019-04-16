f007c41692cbe70149ae5e72937f64437881bc21
Amahdya Delkescamp <256523@via.dk>
2019-04-16 19:26:19 +0200

    Added drawables to the app layout and updated appbar.

--- app/src/main/java/com/example/courseproject/MainActivity.java
+++ app/src/main/java/com/example/courseproject/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -52,5 +54,22 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.settingsButton:
+                //Intent or fragment leading to settings page here...
+                return true;
+
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
     }
 }
