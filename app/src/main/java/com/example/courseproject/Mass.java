238e82d03b5c90de442304e075d0b11a8180c90b
Amahdya Delkescamp <256523@via.dk>
2019-04-26 19:14:22 +0200

    Inflating toolbars

--- app/src/main/java/com/example/courseproject/Mass.java
+++ app/src/main/java/com/example/courseproject/Mass.java
@@ -1,7 +1,11 @@
 package com.example.courseproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Mass extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class Mass extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mass);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.appToolbarMass);
+        setSupportActionBar(toolbar);
+    }
+
+    public boolean onCreateOptionMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.sub_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.homeIcon:
+                Intent intent = new Intent(Mass.this, MainActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
--- app/src/main/java/com/example/courseproject/Volume.java
+++ app/src/main/java/com/example/courseproject/Volume.java
@@ -1,7 +1,11 @@
 package com.example.courseproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Volume extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class Volume extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volume);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.appToolbarVolume);
+        setSupportActionBar(toolbar);
+    }
+
+    public boolean onCreateOptionMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.sub_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.homeIcon:
+                Intent intent = new Intent(Volume.this, MainActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
