5ff5f6131cac91ae6f1cffc1a6a2bdb204bd34ad
Amahdya Delkescamp <256523@via.dk>
2019-04-28 22:29:46 +0200

    Removed unused imports

--- app/src/main/java/com/example/courseproject/Settings.java
+++ app/src/main/java/com/example/courseproject/Settings.java
@@ -1,12 +1,7 @@
 package com.example.courseproject;
 
-import android.content.Intent;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class Settings extends AppCompatActivity {
 
